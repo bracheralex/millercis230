c586a4f1d25580079d7168ce87cfa9c141ce5cc4
Alex Miller <bracheralex89@gmail.com>
2022-03-17 12:22:44 -0700

    fixed syntax errors

--- MillerCharacters/app/Http/Controllers/CharacterController.php
+++ MillerCharacters/app/Http/Controllers/CharacterController.php
@@ -39,7 +39,7 @@ class CharacterController extends Controller
      */
     public function store(Request $request)
     {
-         $validatedData = $request([
+         $validatedData = $request->validate([
             'name' => 'required',
             'birthday' => 'required',
             'occupation' => 'required',
@@ -87,7 +87,7 @@ class CharacterController extends Controller
      */
     public function update(Request $request, $id)
     {
-            $request([
+            $request->validate([
               'name' => 'required',
             'birthday' => 'required',
             'occupation' => 'required',
